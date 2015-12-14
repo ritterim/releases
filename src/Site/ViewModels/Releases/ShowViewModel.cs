f09d6e6eebd384ec11a2f84c2cab30b382c8e2eb
Kevin Hougasian <kevin.hougasian@ritterim.com>
2015-12-14 16:24:15 -0500

    repo icons on description, responsive columns, title color change

--- src/Site/ViewModels/Releases/ShowViewModel.cs
+++ src/Site/ViewModels/Releases/ShowViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using RimDev.Releases.Models;
+using System.Linq;
 
 namespace RimDev.Releases.ViewModels.Releases
 {
@@ -15,6 +16,7 @@ namespace RimDev.Releases.ViewModels.Releases
         public GitHubRepository CurrentRepository { get; set; }
         public AppSettings AppSettings { get; set; }
         public IList<ReleaseViewModel> Releases { get; set; }
+        public bool NotEmpty => Releases != null && Releases.Any();
 
         public int Page { get; set; }
         public int PageSize { get; set; }
